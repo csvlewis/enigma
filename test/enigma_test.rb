86b8f619d780fff0d8078688b620b510518a7f98
Chris Lewis <13chlewis@gmail.com>
2019-01-05 13:02:45 -0700

    Encryption with key and date method and test working

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -7,4 +7,15 @@ class EnigmaTest < Minitest::Test
 
     assert_instance_of Enigma, enigma
   end
+
+  def test_it_can_encrypt_messages_with_key_and_date
+    enigma = Enigma.new
+    hash = {
+    encryption: "keder ohulw!",
+    key: "02715",
+    date: "040895"
+  }
+  
+    assert_equal hash, enigma.encrypt("hello world!", "02715", "040895")
+  end
 end
