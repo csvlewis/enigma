fd4f7a0c00d76c3a72a23551f36c89022c2805a4
Chris Lewis <13chlewis@gmail.com>
2019-01-07 09:57:54 -0700

    Created initial cracker tests

--- test/cracker.test.rb
+++ test/cracker.test.rb
@@ -7,8 +7,24 @@ class CrackerTest < Minitest::Test
   def test_it_exists
     offset = Offset.new("040895")
     cracker = Cracker.new("keder ohulw!", offset)
+
+    assert_instance_of Cracker, cracker
+  end
+
+  def test_it_can_crack_key_with_a_date
+    offset = Offset.new("040895")
+    cracker = Cracker.new("keder ohulw!", offset)
     cracker.crack
 
     assert_equal "02715", cracker.key
+  end
+
+  def test_it_can_crack_key_with_todays_date
+    enigma = Engima.new
+    encrypted = enigma.encrypt("hello world!")
+    cracker = Cracker.new(encrypted[:encryption])
+    cracker.crack
+
+    assert_equal encrypted[:key], cracker.key
   end
 end
