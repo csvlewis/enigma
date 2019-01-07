ffb8b8b4d1ea2209624c23c6e4b05da6c383e075
Chris Lewis <13chlewis@gmail.com>
2019-01-07 11:32:13 -0700

    Continuing cracker class, attempting to get key value from shifts

--- lib/cracker.rb
+++ lib/cracker.rb
@@ -4,30 +4,39 @@ class Cracker
     @message_array = message.split("")
     @last_characters = @message_array[-4..-1]
     @offset = offset
-    @letter_shifts = []
+    @shifts = []
   end
 
   def crack
-    calculate_shift(@last_characters[0], " ")
-    calculate_shift(@last_characters[1], "e")
-    calculate_shift(@last_characters[2], "n")
-    calculate_shift(@last_characters[3], "d")
-
+    gather_shifts
+    rotate_shifts
+    require 'pry'; binding.pry
   end
 
-  def assign_shifts
+  def gather_shifts
+    @shifts << calculate_shift(@last_characters[0], " ")
+    @shifts << calculate_shift(@last_characters[1], "e")
+    @shifts << calculate_shift(@last_characters[2], "n")
+    @shifts << calculate_shift(@last_characters[3], "d")
+  end
+
+  def rotate_shifts
     if @message_array.count % 4 == 0
     elsif @message_array.count % 4 == 3
-      @letter_shifts.rotate
+      @shifts = @shifts.rotate
     elsif @message_array.count % 4 == 2
-      @letter_shifts.rotate.rotate
+      @shifts = @shifts.rotate.rotate
     elsif @message_array.count % 4 == 1
-      @letter_shifts.rotate.rotate.rotate
+      @shifts = @shifts.rotate.rotate.rotate
     end
   end
 
   def calculate_shift(encrypted, original)
-    shift = 27 - (@alphabet.index(encrypted) - @alphabet.index(original)).abs
-    require 'pry'; binding.pry
+    shift = @alphabet.index(encrypted) - @alphabet.index(original)
+    if shift.negative?
+      shift + 27
+    else
+      shift
+    end
   end
 end
