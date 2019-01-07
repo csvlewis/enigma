989735fa1274dacf9c1e371f064d361ab15e0d90
Chris Lewis <13chlewis@gmail.com>
2019-01-07 13:43:04 -0700

    Initial tests with cracker passing

--- lib/cracker.rb
+++ lib/cracker.rb
@@ -1,16 +1,26 @@
+require './lib/key'
+
 class Cracker
+  attr_reader :alphabet,
+              :message_array,
+              :last_characters,
+              :offset,
+              :shifts,
+              :key
   def initialize(message, offset)
     @alphabet = ("a".."z").to_a << " "
     @message_array = message.split("")
     @last_characters = @message_array[-4..-1]
     @offset = offset
     @shifts = []
+    @key = nil
   end
 
   def crack
     gather_shifts
     rotate_shifts
-    require 'pry'; binding.pry
+    subtract_offsets
+    @key = generate_key
   end
 
   def gather_shifts
@@ -37,6 +47,27 @@ class Cracker
       shift + 27
     else
       shift
+    end
+  end
+
+  def subtract_offsets
+    @shifts[0] = @shifts[0] - @offset.offset_a
+    @shifts[1] = @shifts[1] - @offset.offset_b
+    @shifts[2] = @shifts[2] - @offset.offset_c
+    @shifts[3] = @shifts[3] - @offset.offset_d
+  end
+
+  def generate_key
+    loop do
+      key = Key.new(rand(10 ** 5).to_s.rjust(5,'0'))
+      if key.subkey_a % 27 == @shifts[0] &&
+         key.subkey_b % 27 == @shifts[1] &&
+         key.subkey_c % 27 == @shifts[2] &&
+         key.subkey_d % 27 == @shifts[3]
+        return key
+      else
+        next
+      end
     end
   end
 end
--- test/cracker.test.rb
+++ test/cracker.test.rb
@@ -2,29 +2,31 @@ require './test/test_helper'
 require './lib/cracker'
 require './lib/key'
 require './lib/offset'
+require './lib/enigma'
 
 class CrackerTest < Minitest::Test
   def test_it_exists
-    offset = Offset.new("040895")
-    cracker = Cracker.new("keder ohulw!", offset)
+    offset = Offset.new("291018")
+    cracker = Cracker.new("vjqtbeaweqihssi", offset)
 
     assert_instance_of Cracker, cracker
   end
 
   def test_it_can_crack_key_with_a_date
-    offset = Offset.new("040895")
-    cracker = Cracker.new("keder ohulw!", offset)
+    offset = Offset.new("291018")
+    cracker = Cracker.new("vjqtbeaweqihssi", offset)
     cracker.crack
 
-    assert_equal "02715", cracker.key
+    assert_equal "08304", cracker.key.key
   end
 
   def test_it_can_crack_key_with_todays_date
-    enigma = Engima.new
-    encrypted = enigma.encrypt("hello world!")
-    cracker = Cracker.new(encrypted[:encryption])
+    enigma = Enigma.new
+    encrypted = enigma.encrypt("hello world end", "12345")
+    offset = Offset.new(Time.now.strftime("%d%m%y"))
+    cracker = Cracker.new(encrypted[:encryption], offset)
     cracker.crack
 
-    assert_equal encrypted[:key], cracker.key
+    assert_equal encrypted[:key], cracker.key.key
   end
 end
