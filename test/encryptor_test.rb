70b4bf1e0bdebba8691fa7364fd98b46ce99d261
Chris Lewis <13chlewis@gmail.com>
2019-01-07 09:51:46 -0700

    Wrote additional tests for encryptor

--- test/encryptor_test.rb
+++ test/encryptor_test.rb
@@ -13,21 +13,61 @@ class EncryptorTest < Minitest::Test
   end
 
   def test_it_has_an_alphabet
+    key = Key.new("02715")
+    offset = Offset.new("040895")
+    encryptor = Encryptor.new("hello world!", key, offset)
+
+    assert_equal ("a".."z").to_a << " ", encryptor.alphabet
   end
 
   def test_it_can_calculate_shifts
+    key = Key.new("02715")
+    offset = Offset.new("040895")
+    encryptor = Encryptor.new("hello world!", key, offset)
+
+    assert_equal 3, encryptor.shift_a
+    assert_equal 27, encryptor.shift_b
+    assert_equal 73, encryptor.shift_c
+    assert_equal 20, encryptor.shift_d
   end
 
   def test_it_has_a_message_array
+    key = Key.new("02715")
+    offset = Offset.new("040895")
+    encryptor = Encryptor.new("hello world!", key, offset)
+
+    assert_equal 12, encryptor.message_array.count
   end
 
   def test_it_starts_without_a_new_message
+    key = Key.new("02715")
+    offset = Offset.new("040895")
+    encryptor = Encryptor.new("hello world!", key, offset)
+
+    assert_equal [], encryptor.new_message
   end
 
   def test_loop_and_shift_num_start_at_0
+    key = Key.new("02715")
+    offset = Offset.new("040895")
+    encryptor = Encryptor.new("hello world!", key, offset)
+
+    assert_equal 0, encryptor.loop_num
+    assert_equal 0, encryptor.shift_num
   end
 
   def test_increase_shift_and_loop_count
+    key = Key.new("02715")
+    offset = Offset.new("040895")
+    encryptor = Encryptor.new("hello world!", key, offset)
+
+    encryptor.increase_count
+    encryptor.increase_count
+    encryptor.increase_count
+    encryptor.increase_count
+
+    assert_equal 4, encryptor.loop_num
+    assert_equal 0, encryptor.shift_num
   end
 
   def test_it_can_encrypt_messages
